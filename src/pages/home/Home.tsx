0fb2d72453dfd6effc43778acaea48eb93b08fa0
Luci Abreu <luciara.abreu@gmail.com>
2024-04-18 18:27:33 -0300

    update home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,8 @@
-import Masonry from "../../components/grid/Masonry.grid";
+import Banner from "../../components/banner/Banner";
 import NavbarComponent from "../../components/navbar/Navbar";
 import ParalaxVertical from "../../components/paralax/ParalaxVertical";
 import About from "../about/About";
+import ActivitiesSection from "../activitiesSection/ActivitiesSection";
 import { BodyHome, ContainerHome } from "./styles";
 
 
@@ -9,11 +10,11 @@ function Home() {
   return (
     <ContainerHome>
       <NavbarComponent />
-      <BodyHome id='home'>primeira parte do site</BodyHome>
-      <BodyHome id='projeto'>segunda parte do site</BodyHome>
+      <BodyHome id='home'><Banner /></BodyHome>
+      <BodyHome id='projeto'><ActivitiesSection /></BodyHome>
       <BodyHome id='sobre'><About /></BodyHome>
       <BodyHome id='info'>quarta parte do site</BodyHome>
-      <BodyHome id='info2'><Masonry /></BodyHome>
+      <BodyHome id='info2'>quinta parte do site</BodyHome>
       <BodyHome id='libary'><ParalaxVertical /></BodyHome> 
     </ContainerHome>
   )
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -4,12 +4,13 @@ import styled from 'styled-components';
 export const ContainerHome = styled.div`
 width: 100%;
 height: 100%;
-overflow: hidden;
+margin:0;
+padding:0;
 `
 
 export const BodyHome = styled.div`
 width: 100%;
-height: 95vh;
+height: 100vh;
 background: #9c93a3;
 
 `
